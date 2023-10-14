77c3ff6ffcde4d996b3aef2547ba79c61f84505a
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-14 16:31:43 +0800

    fix: prisma client fix

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = {
-    prisma: PrismaClient | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
-
-
-if (process.env.NODE_ENV !== 'production')
-    globalForPrisma.prisma = prisma
+const prisma = new PrismaClient();
 
 export default prisma;

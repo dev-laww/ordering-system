35bee760c2025cc2f03d25ef7eebb0ef1ee9ecff
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-14 16:32:20 +0800

    feat: finished routing auth endpoints

---
+++ src/app/api/auth/forgot-password/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+import AuthController from "@controller/auth.controller";
+
+
+export async function handler(req) {
+    const controller = new AuthController();
+    const { status, response } = await controller.forgotPassword(req);
+
+    return NextResponse.json(response, { status })
+}
+
+export {
+    handler as POST
+}

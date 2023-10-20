a3c6849449d6b1d79165a8b83cd69a45bc446c8c
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-20 20:04:21 +0800

    feat: created register page

---
+++ src/app/auth/register/page.jsx
@@ -0,0 +1,13 @@
+import { Register } from '@components/forms';
+
+
+export const metadata = {
+    title: 'Register',
+    description: 'Register page',
+};
+
+export default function RegisterPage() {
+    return (
+        <Register />
+    );
+}

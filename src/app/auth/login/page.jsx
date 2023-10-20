d088a7e077eb3e4833492f6667c3415502836a21
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-20 20:04:10 +0800

    feat: created login page

---
+++ src/app/auth/login/page.jsx
@@ -0,0 +1,13 @@
+import { Login } from '@components/forms'
+
+export const metadata = {
+    title: 'Login',
+    description: 'Login to your account'
+}
+
+
+export default function LoginPage() {
+    return (
+        <Login/>
+    )
+}

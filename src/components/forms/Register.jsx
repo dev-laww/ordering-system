b1155788cfd73868c0b2839b1a165a11f5b0a5b7
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-20 20:02:28 +0800

    feat: created register form

---
+++ src/components/forms/Register.jsx
@@ -0,0 +1,191 @@
+'use client'
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { IconLock } from '@tabler/icons-react';
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function Register() {
+    const [errors, setErrors] = React.useState({})
+    const [loading, setLoading] = React.useState(false)
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    React.useEffect(() => {
+        if (!session) return
+
+        router.push('/')
+    }, [router, session])
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        setErrors({})
+
+        const data = new FormData(event.currentTarget);
+        const credentials ={
+            firstName: data.get('firstName'),
+            lastName: data.get('lastName'),
+            email: data.get('email'),
+            password: data.get('password'),
+            confirmPassword: data.get('confirmPassword'),
+        };
+
+        const valid = Object.keys(credentials).every(key => credentials[key] !== '');
+
+        if (!valid) {
+            Object.keys(credentials).forEach(key => {
+                if (credentials[key] === '') setErrors(prev => ({ ...prev, [key]: 'Required' }))
+            })
+
+            return
+        }
+
+        if (credentials.password !== credentials.confirmPassword) {
+            setErrors({ confirmPassword: 'Passwords do not match' });
+            return;
+        }
+
+        setLoading(true)
+
+        try {
+            const res = await fetch('/api/auth/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(credentials),
+            });
+
+            const json = await res.json();
+
+            if (!res.ok) {
+                switch (json.message) {
+                    case 'User already exists':
+                        setErrors({ email: 'User already exists' });
+                        break;
+                }
+
+                throw new Error('Something went wrong!');
+            }
+
+            setLoading(false);
+
+            router.push('/auth/login')
+        } catch (err) {
+            console.log(err);
+        }
+
+        setLoading(false)
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+                    <IconLock/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                error={!!errors.firstName}
+                                helperText={errors.firstName}
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="lastName"
+                                label="Last Name"
+                                name="lastName"
+                                autoComplete="family-name"
+                                error={!!errors.lastName}
+                                helperText={errors.lastName}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                error={!!errors.email}
+                                helperText={errors.email}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                                error={!!errors.password}
+                                helperText={errors.password}
+                            />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="confirmPassword"
+                                label="Confirm Password"
+                                type="password"
+                                id="confirm-password"
+                                autoComplete="new-password"
+                                error={!!errors.confirmPassword}
+                                helperText={errors.confirmPassword}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        disabled={loading}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="/auth/login" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
+    );
+}

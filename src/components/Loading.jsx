e543cfd114923056a142c59b0a77ec428776a7a0
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-23 18:26:22 +0800

    feat: created loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
+export default function Loading() {
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+        }}>
+            <CircularProgress />
+        </Box>
+    );
+}

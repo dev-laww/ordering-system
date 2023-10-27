49f6bcfdb78f3d7575f84c36d98ca12ce6968ca1
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-27 16:00:44 +0800

    feat: added admin actions

--- src/app/orders/[id]/page.js
+++ src/app/orders/[id]/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { Box, Button, Chip, Divider, Typography } from '@mui/material';
+import { Box, Button, Chip, CircularProgress, Divider, Typography } from '@mui/material';
 import { ERROR_CODE } from "@lib/constants";
 import Loading from "@components/Loading";
 import PageContainer from "@components/common/PageContainer";
@@ -8,16 +8,139 @@ import { useFetch } from "@lib/hooks";
 import { useSession } from "next-auth/react";
 import { OrderItems } from "@components/tables/";
 import { StatusChip } from "@components/common/";
+import * as React from "react";
+import { useState } from "react";
+import { refreshToken } from "@lib/http";
 
 export default function Order({ params }) {
     const { data: session, status } = useSession();
     const [data, loading, error] = useFetch(`/api/orders/${params.id}`, {}, status);
+    const [buttonLoading, setButtonLoading] = useState({});
 
     if (loading || status === 'loading' || !Boolean(data.data)) return <Loading/>;
 
     if (data.code && data.code === ERROR_CODE.NOT_FOUND) return <PageContainer title="Order Not Found"/>;
 
     const { data: order } = data;
+    const pending = order.status === 'pending';
+
+    const handleCancel = async e => {
+        e.preventDefault();
+
+        setButtonLoading(prev => ({ ...prev, cancel: true }));
+
+        let response = await fetch(`/api/orders/${params.id}/cancel`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': session.accessToken
+            },
+            body: JSON.stringify({ reason: 'Cancelled by admin' }),
+        });
+        const data = await response.json();
+
+        if (data.message === 'Invalid access token') {
+            // refresh token
+            const accessToken = await refreshToken(session.refreshToken);
+
+            response = await fetch(`/api/orders/${params.id}/cancel`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({ reason: 'Cancelled by admin' }),
+            });
+        }
+
+        setButtonLoading(prev => ({ ...prev, cancel: false }));
+
+        if (!response.ok) return;
+
+        if (data.code && data.code === ERROR_CODE.NOT_FOUND) return;
+
+        window.location.reload();
+    }
+
+    const handleComplete = async e => {
+        e.preventDefault();
+
+        setButtonLoading(prev => ({ ...prev, complete: true }));
+
+        let response = await fetch(`/api/orders/${params.id}/complete`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': session.accessToken
+            },
+            body: JSON.stringify({ reason: 'Completed by admin' }),
+        });
+        const data = await response.json();
+
+        if (data.message === 'Invalid access token') {
+            // refresh token
+            const accessToken = await refreshToken(session.refreshToken);
+
+            response = await fetch(`/api/orders/${params.id}/complete`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({ reason: 'Completed by admin' }),
+            });
+        }
+
+        setButtonLoading(prev => ({ ...prev, complete: false }));
+
+        if (!response.ok) return;
+
+
+        if (data.code && data.code === ERROR_CODE.NOT_FOUND) return;
+
+        window.location.reload();
+    }
+
+    const handlePaid = async e => {
+        e.preventDefault();
+
+        setButtonLoading(prev => ({ ...prev, paid: true }));
+
+        let response = await fetch(`/api/payments/${order.payment.id}/complete`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': session.accessToken
+            },
+            body: JSON.stringify({ reason: 'Paid by admin' }),
+        });
+        const data = await response.json();
+
+        if (data.message === 'Invalid access token') {
+            // refresh token
+            const accessToken = await refreshToken(session.refreshToken);
+
+            response = await fetch(`/api/payments/${order.payment.id}/complete`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({ reason: 'Cancelled by admin' }),
+            });
+        }
+
+
+        setButtonLoading(prev => ({ ...prev, paid: false }));
+
+        if (!response.ok) return;
+
+
+        if (data.code && data.code === ERROR_CODE.NOT_FOUND) return;
+
+
+        window.location.reload();
+    }
 
 
     return (
@@ -42,17 +165,18 @@ export default function Order({ params }) {
                     </Divider>
                     <Typography mt={2}>
                         Status: {
-                            <StatusChip status={order.status}/>
-                        }
+                        <StatusChip status={order.status}/>
+                    }
                     </Typography>
                     <Typography mt={2}>Total: {order.total}</Typography>
                     <Typography my={2}>Created at: {order.createdAt}</Typography>
-                    {order.reason && <Typography my={2}>Reason: {order.reason}</Typography>}
+                    {order.reason && <Typography mb={2}>Reason: {order.reason}</Typography>}
                     {order.payment && (
                         <>
                             <Divider>
                                 <Typography color="gray">Payment Details</Typography>
                             </Divider>
+                            <Typography mt={2}>ID: {order.payment.id}</Typography>
                             <Typography mt={2}>Type: {
                                 order.payment.type
                                     .charAt(0)
@@ -111,9 +235,27 @@ export default function Order({ params }) {
                             gap={2}
                             my={2}
                         >
-                            <Button variant='contained'>Cancel</Button>
-                            <Button variant='contained'>Mark as completed</Button>
-                            <Button variant='contained'>Mark as paid</Button>
+                            <Button
+                                variant='contained'
+                                disabled={!pending || buttonLoading.cancel}
+                                onClick={handleCancel}
+                            >
+                                {buttonLoading.cancel ? <CircularProgress size={24}/> : 'Cancel'}
+                            </Button>
+                            <Button
+                                variant='contained'
+                                disabled={!pending || buttonLoading.complete}
+                                onClick={handleComplete}
+                            >
+                                {buttonLoading.complete ? <CircularProgress size={24}/> : 'Mark as complete'}
+                            </Button>
+                            <Button
+                                variant='contained'
+                                disabled={order.payment?.status !== 'pending' || !pending || buttonLoading.paid}
+                                onClick={handlePaid}
+                            >
+                                {buttonLoading.paid ? <CircularProgress size={24}/> : 'Mark as paid'}
+                            </Button>
                         </Box>
                     </>
                 )}

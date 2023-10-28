4bd1696a07c9331c9c34351af4741374cb911b1a
tora-o <97511605+tora-o@users.noreply.github.com>
2023-10-28 13:32:01 +0800

    feat: created middleware

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(request) {
+    const { pathname } = request.nextUrl
+
+    const session = await getToken({ req: request });
+
+    if (session.role !== "admin" && pathname === "/items") {
+        return NextResponse.rewrite(new URL("/not-found", request.nextUrl))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: "/((?!_next/static|_next/image|favicon.ico|api|auth/*).*)",
+}

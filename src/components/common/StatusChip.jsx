5c3c53d289b3ef8202a11c1adf97ff78a7ff7cd5
tora-o <97511605+tora-o@users.noreply.github.com>
2023-11-26 15:03:39 +0800

    feat: edited status color

--- src/components/common/StatusChip.jsx
+++ src/components/common/StatusChip.jsx
@@ -1,7 +1,7 @@
 import { Chip } from '@mui/material';
 
 export default function StatusChip({ status }) {
-    const color = status === "completed" ? "primary.main" : status === "cancelled" ? "error.main" : "warning.main";
+    const color = status === "completed" ? "success.main" : status === "cancelled" ? "error.main" : "warning.main";
     const label = status.charAt(0).toUpperCase() + status.slice(1);
 
     return <Chip

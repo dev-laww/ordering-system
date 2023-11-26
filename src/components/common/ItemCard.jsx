4c162b4fdf929826f844382dafb8089e62201d8f
tora-o <97511605+tora-o@users.noreply.github.com>
2023-11-26 14:43:59 +0800

    feat: added image and enhanced card

--- src/components/common/ItemCard.jsx
+++ src/components/common/ItemCard.jsx
@@ -1,30 +1,29 @@
 import * as React from 'react';
-import { Box, Button, Card, CardActions, CardContent, Typography, Link } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardMedia, Link, Typography } from '@mui/material';
 
 
 export default function ItemCard({ item }) {
     return (
-        <Card sx={{ minWidth: 250 }}>
-            <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    {item.name}
-                </Typography>
-                <Typography variant="h5" component="div">
-                    {item.price}
-                </Typography>
-                <Typography variant="body2">
-                    {item.size}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    size="small"
-                    component={Link}
-                    href={`/items/${item.id}`}
-                >
-                    View Details
-                </Button>
-            </CardActions>
+        <Card sx={ { minWidth: 250 } }>
+            <CardActionArea component={Link} href={`/items/${item.id}`}>
+                <CardContent>
+                    <CardMedia
+                        component="img"
+                        image="/egg.jpg"
+                        title="Egg"
+                        sx={ { height: 200, marginBottom: 3 } }
+                    />
+                    <Typography sx={ { fontSize: 14 } } color="text.secondary" gutterBottom>
+                        { item.name }
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        { item.price }
+                    </Typography>
+                    <Typography variant="body2">
+                        { item.size }
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 }
